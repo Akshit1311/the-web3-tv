749642f10429e3363a8f97390eabba85164bb0c6
harry-sketch <harshbisht90@gmail.com>
2024-04-15 23:20:13 +0530

    add: roomId & token

--- apps/nextjs/src/app/api/route.ts
+++ apps/nextjs/src/app/api/route.ts
@@ -2,21 +2,19 @@ import { AccessToken, Role } from "@huddle01/server-sdk/auth";
 
 export const dynamic = "force-dynamic";
 
-export default async function GET(request: Request) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
   const roomId = searchParams.get("roomId");
 
   if (!roomId) {
-    return new Response("throw new error");
+    return new Response("Missing roomId", { status: 400 });
   }
 
   const accessToken = new AccessToken({
     apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "",
-    roomId,
-    //available roles: Role.HOST, Role.CO_HOST, Role.SPEAKER, Role.LISTENER, Role.GUEST - depending on the privileges you want to give to the user
+    roomId: roomId,
     role: Role.HOST,
-    //custom permissions give you more flexibility in terms of the user privileges than a pre-defined role
     permissions: {
       admin: true,
       canConsume: true,
@@ -34,5 +32,7 @@ export default async function GET(request: Request) {
 
   const token = await accessToken.toJwt();
 
-  return new Response(token);
+  return new Response(token, {
+    status: 200,
+  });
 }

cc24b1b569dcf11d6063fbf9d5988378539bb38b
anjanah07 <thanjana7@gmail.com>
2024-04-20 22:42:26 +0530

    format code

--- apps/nextjs/src/app/_components/Provider/Provider.tsx
+++ apps/nextjs/src/app/_components/Provider/Provider.tsx
@@ -2,31 +2,32 @@
 
 import { AuthKitProvider } from "@farcaster/auth-kit";
 import { HuddleClient, HuddleProvider } from "@huddle01/react";
-
-import { farcasterConfig } from "~/app/_config/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { farcasterConfig } from "~/app/_config/config";
+
 interface ProviderProps {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Provider: React.FC<ProviderProps> = ({ children }) => {
-	const huddleClient = new HuddleClient({
-		projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? "",
-		options: {
-			activeSpeakers: {
-				size: 8,
-			},
-		},
-	});
-	const queryClient = new QueryClient();
-	return (
-		<QueryClientProvider client={queryClient}>
-			<AuthKitProvider config={farcasterConfig}>
-				<HuddleProvider client={huddleClient}>{children}</HuddleProvider>
-			</AuthKitProvider>
-			<ReactQueryDevtools />
-		</QueryClientProvider>
-	);
+  const huddleClient = new HuddleClient({
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID ?? "",
+    options: {
+      activeSpeakers: {
+        size: 8,
+      },
+    },
+  });
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthKitProvider config={farcasterConfig}>
+        <HuddleProvider client={huddleClient}>{children}</HuddleProvider>
+      </AuthKitProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 };
 export default Provider;
--- apps/nextjs/src/app/_components/Streams/Stream.tsx
+++ apps/nextjs/src/app/_components/Streams/Stream.tsx
@@ -1,10 +1,10 @@
 import Navbar from "~/app/Navbar/Navbar";
 
 const Stream: React.FC = () => {
-	return (
-		<section className="relative h-full">
-			<Navbar />
-		</section>
-	);
+  return (
+    <section className="relative h-full">
+      <Navbar />
+    </section>
+  );
 };
 export default Stream;
